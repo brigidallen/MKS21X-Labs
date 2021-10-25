d6d85c5d5801ece951c305d0aefaae19a3344f02
Brigid Allen <ballen30@stuy.edu>
2021-10-25 10:13:58 -0400

    thing

--- 01-Object/Driver.java
+++ 01-Object/Driver.java
@@ -30,29 +30,3 @@ public class Driver{
     //should print "Mittens is 2 years old."
   }
 }
-
-
-class Kitty{
-  private int age;
-  private String name;
-  public Kitty (){
-    age = 2;
-    name = "Mittens";
-  }
-  public Kitty(int firstAge, String firstName){
-    age = firstAge;
-    name = firstName;
-  }
-  public String getName(){
-    return name;
-  }
-  public int getAge(){
-    return age;
-  }
-  public void changeName(String str){
-    name = str;
-  }
-  public void makeOlder(){
-    age++;
-  }
-}
