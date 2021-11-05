aec6607b0b8a050ec21527ec500b7278b3ea2776
brigidallen <brigidhallen@gmail.com>
2021-11-04 23:35:05 -0400

    Superarray beginning

---
+++ 04-SuperArray/Superarray.java
@@ -0,0 +1,35 @@
+public class Superarray{
+  private String[] data;
+  private int size;
+  private Superarray(){
+    data = new String[10];
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public void add(String){
+    [size] = String;
+    size ++;
+  }
+  public String toString(){
+    String str = "";
+    str += "[";
+    for (int i = 0, i < size, i++){
+      str += [i];
+    } str += "]";
+    return str;
+  }
+  public toStringDebug(){
+    String str = "";
+    str += "[";
+    for (int i = 0, i < length, i++){
+      str += [i];
+    } str += "]";
+    return str;
+  }
+  public static void main(String[] args) {
+    Superarray b = new Superarray();
+
+  }
+}
