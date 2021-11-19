c490e32de0801abeabb7d6d828e1369262e66a33
Brigid Allen <ballen30@stuy.edu>
2021-11-19 10:15:32 -0500

    No idea what's going on

---
+++ 05-ArrayList/UserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class UserInput{
+  public static void main(String[] args) {
+    ArrayList<String> beep = new ArrayList<String>( 10);
+    System.out.println("Enter a non negative number. A negative number will end the program");
+    Scanner newscan = new Scanner(text);
+    while (input.hasNext()){
+      System.out.println(input.next());
+    }
+  }
+}
