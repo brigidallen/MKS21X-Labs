d79a41f5d2b4a2f3645b30bbd0e554d5a7cb91c4
Brigid Allen <ballen30@stuy.edu>
2021-12-06 10:17:10 -0500

    Insertion sort

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -3,31 +3,31 @@ public class Sorts{
   public static void main(String[] args) {
     int[]Onedata = {2, 3, 1, 6, 0, 9, 4};
     int[]Twodata = {0, 1, 3, 2, 6, 4, 7, 20, 3};
-    System.out.println(Arrays.toString(Onedata));
+    //System.out.println(Arrays.toString(Onedata));
     selectionSort(Onedata);
-    System.out.println(Arrays.toString(Onedata));
+    //System.out.println(Arrays.toString(Onedata));
     int[]Randeqdata = new int[10];
     for(int i = 0; i < Randeqdata.length; i++){
       Randeqdata[i] = (int)(Math.random()*10);
     }
-    System.out.println("Randomized data: " + Arrays.toString(Randeqdata));
+    //System.out.println("Randomized data: " + Arrays.toString(Randeqdata));
     selectionSort(Randeqdata);
-    System.out.println("Randomized sorted: " + Arrays.toString(Randeqdata));
+    //System.out.println("Randomized sorted: " + Arrays.toString(Randeqdata));
     int[]Rand2 = new int[10];
     for (int i = 0; i < Rand2.length; i++){
       Rand2[i] = (int)(Math.random()*10);
     }
     Arrays.sort(Rand2);
-    System.out.println("Array sort: " + Arrays.toString(Rand2));
+    //System.out.println("Array sort: " + Arrays.toString(Rand2));
     selectionSort(Rand2);
-    System.out.println("selectionSort: " + Arrays.toString(Rand2));
+    //System.out.println("selectionSort: " + Arrays.toString(Rand2));
     int[]Rand2copy = new int[Rand2.length];
     for(int i = 0; i < Rand2.length; i++){
       Rand2copy[i] = Rand2[Rand2.length-(i+1)];
     }
-    System.out.println(Arrays.toString(Rand2copy));
+    //System.out.println(Arrays.toString(Rand2copy));
     selectionSort(Rand2copy);
-    System.out.println(Arrays.toString(Rand2copy));
+    //System.out.println(Arrays.toString(Rand2copy));
   }
   public static void bubbleSort(int[] data){
     int theend = data.length-1;
@@ -52,6 +52,17 @@ public class Sorts{
       ary[start] = ary[c];
       ary[c] = temporary;
     }
-    System.out.println(Arrays.toString(ary));
+    //System.out.println(Arrays.toString(ary));
+  }
+  public static void insertionSort(int[]ary){
+    for(int i = 1; i < ary.length; i++){
+      int j = i-1;
+      while(ary[i] < ary[j] && j !=0){
+        j--;
+      } int subst = ary[i];
+      for(int k = j; k < i; k++){
+        ary[k] = ary[k+1];
+      } ary[j] = subst;
+    }
   }
 }
