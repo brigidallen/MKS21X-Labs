996ced8d1388aba75dabfe14ebd32e9951b0563c
Brigid Allen <ballen30@stuy.edu>
2021-12-17 10:16:44 -0500

    checked nonullarraylist and did orderedarraylist

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -6,14 +6,31 @@ public class OrderedArrayList<T> extends NoNullArrayList<T>{
     super(startingCapacity);
   }
   @Override
-  public boolean add(int index, T element){
+  public boolean add(T element){
+    super.add(whereToPlace(element), element);
+  }
+  @Override
+  public void add(int index, T element){
     super.add(whereToPlace(element), element);
   }
   private int whereToPlace(T value){
-    for(int i = 0; i < size(); i++){
-      if(value.compareTo(get(i)) < 0){
-        return i;
+    if(size() > 0){
+      for(int i = 0; i < size(); i++){
+        if(value.compareTo(get(i)) < 0){
+          return i;
+        }
       }
+    } else {
+      return 0;
     }
+  }
+  @Override
+  public T set(int index, T element){
+    remove(index);
+    super.add(whereToPlace(element), element);
+  }
+  public static void main(String[] args) {
+    OrderedArrayList newbie = new OrderedArrayList(10);
+
   }
 }
