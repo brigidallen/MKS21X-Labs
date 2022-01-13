9c2b1fe0e6d3f3a953fffb0dd75941a319c9ca7c
Brigid Allen <ballen30@stuy.edu>
2022-01-13 10:16:34 -0500

    I hate this class

--- Border.java
+++ Border.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Border{
   private static final int WIDTH = 80;
   private static final int HEIGHT = 30;
@@ -5,7 +6,7 @@ public class Border{
   private static final int BG_COLOR = Text.BLUE + Text.BACKGROUND;
 
   public static int[] makeRand(int length){
-    int[] randarr = new int[length-1];
+    int[] randarr = new int[length];
     for(int i = 0; i < randarr.length; i++){
       randarr[i] = (int)(Math.random()*100+1);
     } return randarr;
@@ -14,10 +15,19 @@ public class Border{
       System.out.print("\u001b[" + row + ";" + col + "f");
   }
   public static void Border(){
+    Text.go(1, 1);
     for(int i = 0; i < 80; i++){
       System.out.print("_");
     } System.out.println("");
     Text.go(2, 1);
+    for(int i = 0; i < 80; i++){
+      System.out.print("_");
+    } System.out.println("");
+    Text.go(29,1);
+    for(int i = 0; i < 80; i++){
+      System.out.print("_");
+    } System.out.println("");
+    Text.go(30,1);
     for(int i = 0; i < 80; i++){
       System.out.print("_");
     } System.out.println("");
@@ -39,29 +49,41 @@ public class Border{
     for(int i = 3; i < 30; i++){
       Text.go(i, 80);
       System.out.println("|");
-    } Text.go(26,1);
-    for(int i = 0; i < 80; i++){
-      System.out.print("_");
-    } System.out.println("");
-    Text.go(29,1);
-    for(int i = 0; i < 80; i++){
-      System.out.println("_");
-    } System.out.println("");
+    }
   }
   public static void printRand(){
     int[] randarr = makeRand(4);
     for(int i = 0; i < randarr.length; i++){
-      Text.go(i*22, 2);
+      Text.go(4, i*20+10);
       if(randarr[i] < 25){
-        System.out.println(Text.RED + randarr[i]);
+        System.out.println(Text.colorize(Integer.toString(randarr[i]), Text.RED));
       } else if(randarr[i] > 75){
-        System.out.println(Text.GREEN + randarr[i]);
+        System.out.println(Text.colorize(Integer.toString(randarr[i]), Text.GREEN));
       } else{
-        System.out.println(Text.WHITE + randarr[i]);
+        System.out.println(Text.colorize(Integer.toString(randarr[i]), Text.WHITE));
       }
     }
   }
   public static void main(String[] args) {
     Border();
+    printRand();
+    Text.go(HEIGHT+1, 1);
+    System.out.print(">");
+    Scanner s = new Scanner(System.in);
+    while(true){
+      String line = s.nextLine();
+      if(line.equals("q") || line.equals("quit")){
+        break;
+      } else if(line.equals("")){
+        Text.clear();
+        Border();
+        printRand();
+      } else{
+        Text.clear();
+        Border();
+        printRand();
+      } Text.go(HEIGHT+1, 1);
+      System.out.print(">");
+    }
   }
 }
